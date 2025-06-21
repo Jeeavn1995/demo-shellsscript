93e9cc09203d20ff4b370a325a583bfc9537f734
Ubuntu <ubuntu@ip-172-31-10-225.ap-south-1.compute.internal>
2025-06-21 09:32:49 +0000

    added simplescript

---
+++ simplescript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#
+
+mkdir ramjeevan
+
+touch manikarnika.txt
+
+
+echo "manikarnia ghat is located at the bank of river ganga in varanasi" > manikarnika.txt
+
+echo "this is my test script" >> manikarnika.txt
